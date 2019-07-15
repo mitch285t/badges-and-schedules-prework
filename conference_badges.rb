a4c7403156a8a89fd48ca62c196f3d886c6266c2
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-15 01:20:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,11 @@
 
-
+array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(array)
+  
   
   
 end
